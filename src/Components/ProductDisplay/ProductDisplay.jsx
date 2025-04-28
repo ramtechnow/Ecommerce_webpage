bda23367a961bf3a2425bf89f1ab132a7a1fa2b8
ramtechnow20 <bvhss20@gmail.com>
2025-04-28 14:50:53 +0530

    date_28_04

---
+++ src/Components/ProductDisplay/ProductDisplay.jsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import './ProductDisplay.css'
+import star_icon from '../Assets/star_icon.png'
+import star_dall_icon from '../Assets/star_dull_icon.png'
+
+export const ProductDisplay = (props) => {
+    const { product } = props;
+    return (
+        <div className='productdisplay'>
+            <div className="productdisplay-left">
+                <div className="productdisplay-img-list">
+                    <img src={product.image} alt="" />
+                    <img src={product.image} alt="" />
+                    <img src={product.image} alt="" />
+                    <img src={product.image} alt="" />
+                </div>
+                <div className="productdisplay-img">
+                    <img className='productdisplay-main-img' src={product.image} alt="" />
+                </div>
+            </div>
+            <div className="productdisplay-right">
+                <h1>{product.name}</h1>
+                <div className="productdisplay-right-star">
+                    <img src={star_icon} alt="" />
+                    <img src={star_icon} alt="" />
+                    <img src={star_icon} alt="" />
+                    <img src={star_icon} alt="" />
+                    <img src={star_dall_icon} alt="" />
+                    <p>(122)</p>
+                </div>
+                <div className="productdisplay-right-prices">
+                    <div className="productdisplay-right-price-old">${product.old_price}</div>
+                    <div className="productdisplay-right-price-new">${product.new_price}</div>
+                </div>
+                <div className="productdisplay-right-discription">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim magni commodi cum officiis natus ut numquam in eveniet quidem. Repellendus quo repudiandae.
+                </div>
+                <div className="productdisplay-right-size">
+                    <h1>Select Size</h1>
+                    <div className="productdisplay-right-sizes">
+                        <div>S</div>
+                        <div>M</div>
+                        <div>L</div>
+                        <div>XL</div>
+                        <div>XXL</div>
+                    </div>
+                </div>
+                <button>ADD TO CART</button>
+                <p className='productdisplay-right-categroy'> <span>Categroy :</span>Women , T-shrot, Crop Top</p>
+                <p className='productdisplay-right-categroy'> <span>Tags :</span> Modern , Latest</p>
+            </div>
+        </div>
+    )
+}
