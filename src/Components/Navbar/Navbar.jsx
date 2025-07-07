170660ef3bd3bf940245e3ffb1af66d8082ff2bf
ramtechnow20 <bvhss20@gmail.com>
2025-07-07 09:00:22 +0530

    add

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,21 +1,28 @@
 import './Navbar.css';
 import logo from '../Assets/logo.png';
 import cart_icon from '../Assets/cart_icon.png';
-import { useState ,useContext} from "react";
+import { useState ,useContext, useRef} from "react";
 import { Link } from "react-router-dom";
 import { ShopContext } from '../../Context/ShopContext';
+import nav_dropdown from '../Assets/dropdown_icon.png';
 
 const Navbar = () => {
     const [menu, setMenu] = useState("shop");
     const { getDefaultCartItems } = useContext(ShopContext);
+    const menuRef = useRef();
     
+    const dropdown_toggle = (e) =>{
+        menuRef.current.classList.toggle('nav-menu-visible');
+        e.target.classList.toggle('open');
+    }
     return (
         <div className="navbar">
             <div className="nav-logo">
                 <img src={logo} alt="Shop Logo" />
                 <p>SHOPPER</p>
             </div>
-            <ul className="nav-menu">
+            <img className='nav-dropdown' onClick={dropdown_toggle} src={nav_dropdown} alt="" />
+            <ul ref={menuRef} className="nav-menu">
                 <li onClick={() => { setMenu("shop") }}>
                     <Link style={{ textDecoration: 'none' }} to='/'>Shop</Link>
                     {menu === "shop" ? <hr /> : <></>}
